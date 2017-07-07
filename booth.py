fae6c10d980ce6c531efa51f9185b8f348a693aa
Brent <brenthaas@gmail.com>
2017-07-07 01:29:05 -0700

    get the booth working

--- booth.py
+++ booth.py
@@ -1,12 +1,26 @@
 import photo_taker
 import picamera
+import poofer
 import smart_led
 import sys
+import time
 import RPi.GPIO as GPIO
 
 def setup_button(pin, callback):
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback, bouncetime=800)
+
+def milli_time(time_float):
+    return int(round(time_float * 1000))
+
+def take_picture():
+    GPIO.output(slr_pin, GPIO.LOW)
+    time.sleep(.3)
+    GPIO.output(slr_pin, GPIO.HIGH)
+
+def setup_slr(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)  # turn off
 
 def setup_camera(camera):
     camera.rotation = 270
@@ -29,6 +43,10 @@ GPIO.setmode(GPIO.BCM)
 
 taking_photo = False
 taker = None
+camera_delay = 20
+poof_start_at = None
+
+slr_pin = 20
 
 try:
     # setup LEDs
@@ -40,15 +58,34 @@ try:
     button_pin = 25
     setup_button(button_pin, take_photo)
 
+    poofer_pin = 16
+    poofer = poofer.Poofer(
+        pin= poofer_pin,
+        flame_duration_ms= 20,
+        callback= take_picture
+    )
+    poofer.setup()
+
+    setup_slr(slr_pin)
+
     with picamera.PiCamera() as camera:
         setup_camera(camera)
 
         while True:
+            poofer.update()
             button_led.update()
+
+            # if poof_start_at != None:
+            #     delta = milli_time(time.time()) - poof_start_at
+            #     if delta > camera_delay:
+            #         poof_start_at = None
+            #         take_picture(slr_pin)
 
             if taking_photo:
                 taker = taker or setup_taker(camera)
                 if taker.is_done():
+                    poofer.poof()
+                    poof_start_at = milli_time(time.time())
                     taker.shutdown()
                     taker = None
                     taking_photo = False
