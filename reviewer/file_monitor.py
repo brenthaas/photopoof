fdcf41f06fbc203a8e31ece5947dd52e46eb4664
Brent Haas <brent.haas@bugcrowd.com>
2019-04-02 23:32:49 -0700

    create file monitor

---
+++ reviewer/file_monitor.py
@@ -0,0 +1,41 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class Watcher:
+    def __init__(self, dir):
+        self.observer = Observer()
+        self.dir_to_watch = dir
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.dir_to_watch, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(500000)
+        except:
+            self.observer.stop()
+            print("Error")
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print("Received created event - %s." % event.src_path)
+
+        elif event.event_type == 'modified':
+            # Taken any action here when a file is modified.
+            print("Received modified event - %s." % event.src_path)
+
+
+if __name__ == '__main__':
+    w = Watcher("/Users/brent/tmp")
+    w.run()
