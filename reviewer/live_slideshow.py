4df387eeed845258942e87dc44eb184962d2c573
Brent Haas <brent.haas@bugcrowd.com>
2019-07-09 23:39:22 -0700

    rename files

--- reviewer/live_slideshow.py
+++ reviewer/live_slideshow.py
@@ -3,7 +3,7 @@ import os
 from file_monitor import Watcher
 from slideshow import Slideshow
 
-class PhotoViewer:
+class LiveSlideshow:
     def __init__(self, dir, display_duration=3500):
         """Cycles through photos in a directory and updates with changes"""
         self.photo_dir = dir
@@ -19,11 +19,14 @@ class PhotoViewer:
         self.slideshow.destroy()
         self.watcher.finish()
 
+    def reset_slideshow(self):
+        self.slideshow.set_images(self.images)
+
     def add_image_and_restart(self, file_path):
         """Adds the given file to the front of the slideshow"""
         if file_path.endswith('.jpg'):
             self.images = [file_path] + self.images[0:4]
-            self.slideshow.set_images(self.images)
+            self.reset_slideshow()
 
     def setup_images(self):
         """Pulls all jpgs from a directory into slideshow"""
@@ -39,7 +42,7 @@ if __name__ == '__main__':
 
     try:
         path = sys.argv[1] if len(sys.argv) > 1 else "/home/pi/Desktop/photopoof"
-        viewer = PhotoViewer(path)
+        viewer = LiveSlideshow(path)
         viewer.start()
     except Exception as ex:
         print("Closing due to exception" + ex)
---
+++ reviewer/slide_controller.py
@@ -0,0 +1,23 @@
+from live_slideshow import LiveSlideshow
+import RPi.GPIO as GPIO
+
+class SlideController:
+    def __init__(self, dir, reset_button, display_duration=3500):
+        """Gives users control over slideshow display"""
+        GPIO.setmode(GPIO.BCM)
+        self.reset_button = reset_button
+        GPIO.add_event_detect(reset_button, FALLING, self.handle_button)
+        GPIO.setup(reset_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.slides = LiveSlideshow(dir, display_duration)
+
+    def handle_button(self):
+        self.slides.reset_slideshow()
+
+if __name__ == '__main__':
+    import sys
+
+    try:
+        path = sys.argv[1] if len(sys.argv) > 1 else "/home/pi/Desktop/photopoof"
+        controller = SlideController(dir=path, reset_button=23)
+    except Exception as ex:
+        print("Closing due to exception" + ex)
