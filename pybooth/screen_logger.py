742a8a126c3f1c40724d3069b2684d1f1ee66ad6
Brent <brenhaas@gmail.com>
2022-06-29 15:43:42 -0700

    use 'wait' as logger default

--- pybooth/screen_logger.py
+++ pybooth/screen_logger.py
@@ -1,18 +1,12 @@
 from machine import Pin, SoftI2C
 import ssd1306
 
-# Start I2C Communication SCL = 4 and SDA = 5 on Wemos Lolin32 ESP32 with built-in SSD1306 OLED
-i2c = SoftI2C(scl=Pin(4), sda=Pin(5))
-oled_width = 128
-oled_height = 64
-oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
-
 class ScreenLogger:
     def __init__(self, row_names=['default']):
         self.names = row_names
         self.messages = dict()
         for name in row_names:
-            intro = "Hello %s"%(name)
+            intro = "%s wait"%(name)
             self.messages[name] = "%-16s" % intro
         self.setup_screen()
         self.render_all()
