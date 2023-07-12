7eaac3706ae09b822209b8941bfd13a2ffcb6724
Brent Haas <brenthaas@gmail.com>
2023-07-12 00:44:07 -0700

    renamed multi-timer example to main_timers

--- poof/src/main_timers.cpp
+++ poof/src/main_timers.cpp
@@ -26,9 +26,9 @@
 #define POOF_PIN 13
 
 #define COUNTDOWN_SPEED 1000        // rate at which 1 countdown tick occurs
-#define POOF_DURATION 300           // duration to keep poofer valve open
-#define PHOTO_DELAY 400             // Time to wait to take photo
-#define SHUTTER_PRESS_DURATION 150  // How long to press the shutter
+#define POOF_DURATION 1000           // duration to keep poofer valve open
+#define PHOTO_DELAY 1000             // Time to wait to take photo
+#define SHUTTER_PRESS_DURATION 1000  // How long to press the shutter
 
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
@@ -46,14 +46,15 @@ hw_timer_t *camera_timer = NULL;
 hw_timer_t *blink_timer = NULL;
 
 // Countdown Variables
-int count = -1;
+volatile int count = -1;
 int previous_count = -1;
-bool poof_in_progress = false;
+volatile bool poof_in_progress = false;
 bool prev_poof_status = false;
-bool lockout = false;
-bool photo_in_progress = false;
-bool photo_delayed = true;
-bool open_shutter = false;
+bool lockout = false;                         //
+volatile bool photo_in_progress = false;      // used to delay camera
+volatile bool photo_delayed = false;          // used to delay camera
+volatile bool photo_taken = false;            // flags to turn off the timer
+volatile bool open_shutter = false;
 
 
 //////////////////////////////
@@ -85,23 +86,32 @@ void display_number(int num)
 void IRAM_ATTR countdown_ISR()
 {
   portENTER_CRITICAL_ISR(&timerMux);
-  --count;
+  count--;
   portEXIT_CRITICAL_ISR(&timerMux);
 }
 
 void IRAM_ATTR camera_ISR()
 {
   portENTER_CRITICAL_ISR(&timerMux);
-  if (photo_delayed)                // First Time Through
-  {
+  if (photo_delayed) {
     photo_delayed = false;
-    digitalWrite(CAMERA_PIN, LOW);  // Open Shutter
-    open_shutter = true;
-  } else if (open_shutter)
+  }
+  // portENTER_CRITICAL_ISR(&timerMux);
+  // if (!photo_in_progress)           // First Time Through
+  // {
+  //   photo_in_progress = true;       // Delay
+  // } else if (!open_shutter)
+  // {
+  //   digitalWrite(CAMERA_PIN, LOW);  // Open Shutter
+  //   open_shutter = true;
+  // } else
+  else
   {
     digitalWrite(CAMERA_PIN, HIGH); // Close Shutter
     open_shutter = false;
+    // photo_taken = false;            // Done
     photo_in_progress = false;
+    photo_taken = true;
   }
   portEXIT_CRITICAL_ISR(&timerMux);
 }
@@ -120,15 +130,16 @@ void IRAM_ATTR poofer_done_ISR()
 void setup_timer(hw_timer_t *timer, void (*timer_callback)(void), int time_in_ms)
 {
   timerAttachInterrupt(timer, timer_callback, false);
+  // timerAlarmWrite(timer, (1000 * time_in_ms), true);
   timerAlarmWrite(timer, (1000 * time_in_ms), true);
-  timerAlarmEnable(timer);
+  // timerAlarmEnable(timer);
 }
 
 void breakdown_timer(hw_timer_t *timer)
 {
+  // timerStop(timer);
   timerAlarmDisable(timer);
-  timerDetachInterrupt(timer);
-  timerStop(timer);
+  // timerDetachInterrupt(timer);
 }
 
 //////////////////////////////
@@ -138,25 +149,44 @@ void poofer_start()
 {
   display_text("Poof!");
   poof_in_progress = true;
-  setup_timer(poofer_timer, &poofer_done_ISR, POOF_DURATION);
+  // setup_timer(poofer_timer, &poofer_done_ISR, POOF_DURATION, false);
+  timerAlarmWrite(poofer_timer, (1000 * POOF_DURATION), false);
+  timerAlarmEnable(poofer_timer);
   timerStart(poofer_timer);
+  // timerRestart(poofer_timer);
   digitalWrite(POOF_PIN, LOW);
 }
 
-void camera_start()
+void photo_delay_start()
 {
-  photo_delayed = true;
   photo_in_progress = true;
-  setup_timer(camera_timer, &camera_ISR, PHOTO_DELAY);
-  timerStart(camera_timer);
+  photo_delayed = true;
+  timerAlarmWrite(camera_timer, (1000 * PHOTO_DELAY), false);
+  timerAlarmEnable(camera_timer);
+  timerStart(poofer_timer);
+  // timerRestart(camera_timer);
+  // setup_timer(camera_timer, &camera_ISR, PHOTO_DELAY, false);
+  // timerStart(camera_timer);
+}
+
+void take_photo()
+{
+  display_text("Photo!");
+  digitalWrite(CAMERA_PIN, LOW); // Open Shutter
+  timerAlarmWrite(camera_timer, (1000 * PHOTO_DELAY), false);
+  timerAlarmEnable(camera_timer);
+  timerStart(poofer_timer);
 }
 
 void countdown_start()
 {
   lockout = true;
   count = 6;
-  setup_timer(countdown_timer, &countdown_ISR, COUNTDOWN_SPEED);
-  timerStart(countdown_timer);
+  timerAlarmWrite(countdown_timer, (1000 * COUNTDOWN_SPEED), true);
+  timerAlarmEnable(countdown_timer);
+  timerRestart(countdown_timer);
+  // setup_timer(countdown_timer, &countdown_ISR, COUNTDOWN_SPEED, true);
+  // timerStart(countdown_timer);
 }
 
 //////////////////////////////
@@ -238,11 +268,13 @@ void setup()
 
   // setup timers
   countdown_timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(countdown_timer, &countdown_ISR, false);
   // setup_timer(countdown_timer, &countdown_ISR, COUNTDOWN_SPEED);
   poofer_timer = timerBegin(1, 80, true);
+  timerAttachInterrupt(poofer_timer, &poofer_done_ISR, false);
   // setup_timer(poofer_timer, &poofer_done_ISR, POOF_DURATION);
   camera_timer = timerBegin(2, 80, true);
-  // setup_timer(camera_timer, &camera_ISR, PHOTO_DELAY);
+  timerAttachInterrupt(camera_timer, &camera_ISR, false);
   // setup_timer(unlock_timer, &lockout_done_ISR, LOCKOUT_DURATION);
 
   screen_init();
@@ -279,7 +311,7 @@ void loop()
     else if (count == 0 && !poof_in_progress)
     {
       poofer_start();
-      camera_start();
+      photo_delay_start();
     }
     else if (count < -5)
     {
@@ -287,7 +319,6 @@ void loop()
       // timerStop(countdown_timer);
       breakdown_timer(countdown_timer);
       breakdown_timer(poofer_timer);
-      breakdown_timer(camera_timer);
       display_photopoof_logo();
     }
     else
@@ -295,6 +326,17 @@ void loop()
       sprintf(display_string, "%d...", 5+count);
       display_text(display_string);
     }
+  }
+
+  if (photo_in_progress && !photo_delayed)
+  {
+    take_photo();
+  }
+
+  if (photo_taken)
+  {
+    breakdown_timer(camera_timer);
+    photo_taken = false;
   }
 
   // Use different timing for shutter than for delay
